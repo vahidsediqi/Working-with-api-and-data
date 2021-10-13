92e96409196a0fdd5987e69e446c7eb64372b705
unknown <you@example.com>
2021-10-13 15:54:51 +0300

    Working with NASA data is finished

--- NASA Data/main.js
+++ NASA Data/main.js
@@ -1,42 +1,40 @@
 // Data from: https://data.giss.nasa.gov/gistemp/
 
-var ctx = document.getElementById('chart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
+chartIt();
+async function chartIt() {
+const data = await getData();
+const ctx = document.getElementById('chart').getContext('2d');
+const title = `From 1880 to 2020`
+const myChart = new Chart(ctx, {
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: data.XS,
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            label: title,
+            data: data.YS,
+            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+            borderColor: ['rgba(255, 99, 132, 1)'],
             borderWidth: 1
         }]
     },
     options: {
         scales: {
             y: {
-                beginAtZero: true
+                ticks: {
+                    callback: function(value, index, values) {
+                        return value + ' °';
+                    }
+                }
             }
         }
     }
 });
 
-const getData = async() => {
+}
+
+async function getData() {
+    const XS = [];
+    const YS = [];   
     const response = await fetch('ZonAnn.Ts+dSST.csv');
     const data = await response.text()
 
@@ -44,9 +42,14 @@ const getData = async() => {
     table.forEach(row => {
         const cols = row.split(',');
         const year = cols[0];
+        XS.push(year);
         const temp = cols[1];
+        YS.push(parseFloat(temp) + 1);
         console.log(year, temp);
-    })
-}
+    });
+    return {
+        XS, 
+        YS
+    };
 
-getData();
+}
