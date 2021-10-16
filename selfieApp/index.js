4e1cb18a755de2162492e08e24d1a29232830b39
vahidsediqi <vsediqi@live.com>
2021-10-16 14:05:04 -0700

    Nedb database installed

--- selfieApp/index.js
+++ selfieApp/index.js
@@ -1,11 +1,18 @@
 
 const express = require ('express');
 const app = express();
+
+// Listening to port 3000 to run the web server
 app.listen(3000, () => console.log('Listening To Port 3000 from nodeJs'));
+
+// Giving limits for response
 app.use(express.json({ limit: '1mb' }));
+
+// Looking for public folder to run the index.html file
 app.use(express.static('public'));
 
-app.post('/api', (request, response) => {
+// making the API ./api is the url (This is the Rout and end point for the api)
+app.post('/myapiendpoint', (request, response) => {
     console.log('I got a request');
     console.log(request.body);
 
