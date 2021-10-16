b77c0c607c5628077438e54f95a7ce5d196f2007
vahidsediqi <vsediqi@live.com>
2021-10-16 12:05:40 -0700

    created new api and resived data from user

--- selfieApp/index.js
+++ selfieApp/index.js
@@ -1,6 +1,18 @@
+
 const express = require ('express');
 const app = express();
 app.listen(3000, () => console.log('Listening To Port 3000 from nodeJs'));
-app.use(express.static('public'))
+app.use(express.json({ limit: '1mb' }));
+app.use(express.static('public'));
 
-console.log('hello from index.js');
+app.post('/api', (request, response) => {
+    console.log('I got a request');
+    console.log(request.body);
+
+    const data = request.body;
+    response.json({
+        status: 'success',
+        latitude: data.lat,
+        logtitude: data.log
+    });
+});
