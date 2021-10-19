642a98381358deda879ad00f771309664ac5b1af
unknown <you@example.com>
2021-10-19 16:13:45 +0300

    db added

--- selfieApp/index.js
+++ selfieApp/index.js
@@ -1,5 +1,7 @@
 
 const express = require ('express');
+const Datastore = require('nedb');
+
 const app = express();
 
 // Listening to port 3000 to run the web server
@@ -11,14 +13,19 @@ app.use(express.json({ limit: '1mb' }));
 // Looking for public folder to run the index.html file
 app.use(express.static('public'));
 
+// creating database
+const database = new Datastore('myDataBase.db');
+database.loadDatabase();
 // making the API ./api is the url (This is the Rout and end point for the api)
-app.post('/myapiendpoint', (request, response) => {
+app.post('/api', (request, response) => {
     console.log('I got a request');
-    console.log(request.body);
-
     const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
     response.json({
         status: 'success',
+        timestamp: timestamp,
         latitude: data.lat,
         logtitude: data.log
     });
